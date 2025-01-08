391b1e8354ef0385b1a3fa0c70871708664f8803
MashalloA <mashalloalim07@gmail.com>
2025-01-08 19:45:14 +0600

    hw

--- movie_app/models.py
+++ movie_app/models.py
@@ -7,6 +7,7 @@ class Director(models.Model):
         return self.name
 
 class Movie(models.Model):
+
     title = models.CharField(verbose_name="Movie Title", max_length=100)
     description = models.TextField(verbose_name="Movie Description", null=True, blank=True)
     duration = models.IntegerField(verbose_name="Movie Duration", default=0)
@@ -16,8 +17,16 @@ class Movie(models.Model):
         return self.title
 
 class Review(models.Model):
+    STARS = (
+        (1 , 1),
+        (2 , 2),
+        (3 , 3),
+        (4 , 4),
+        (5 , 5),
+    )
     text = models.TextField(verbose_name="Review")
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    grade = models.CharField(max_length=10, verbose_name="Grade", choices=STARS, default="*", null=True)
 
     def __str__(self):
         return self.text
--- movie_app/serializers.py
+++ movie_app/serializers.py
@@ -3,19 +3,33 @@ from .models import Director, Movie, Review
 
 
 class DirectorSerializer(serializers.ModelSerializer):
+    movies_count = serializers.SerializerMethodField()
     class Meta:
         model = Director
         fields = '__all__'
 
-class MovieSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Movie
-        fields = '__all__'
-        # exclude - исключение полей
+    def get_movies_count(self, movies):
+        return movies.movie_set.count()
+
 
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
         fields = '__all__'
 
+class MovieSerializer(serializers.ModelSerializer):
+    review = ReviewSerializer(many=True)
+    average_rating = serializers.SerializerMethodField()
+    class Meta:
+        model = Movie
+        fields = 'title description duration director review average_rating'.split()
+        # exclude - исключение полей
+
+    def get_average_rating(self, movie):
+        reviews = movie.reviews.all()
+        if reviews:
+            sum_reviews = sum(int(i.grade) for i in reviews if i.grade.isdigit())
+            average = sum_reviews / len(reviews)
+            return average
+        return None
 
